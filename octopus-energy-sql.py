d64129ad182a3a61676e5a673526597c26ff3cf2
milkandbourbons <47081499+milkandbourbons@users.noreply.github.com>
2023-11-04 10:05:24 +0000

    Update octopus-energy-sql.py

--- octopus-energy-sql.py
+++ octopus-energy-sql.py
@@ -4,86 +4,52 @@ import time
 from datetime import datetime, timedelta
 import mysql.connector as sql
 import schedule
+import _config
 
-'''
-######################################################
-THIS SECTION IS FOR CONFIGURATION OF YOUR ACCOUNT DETAILS.
-API key available at https://octopus.energy/dashboard/new/accounts/personal-details/api-access
-######################################################
-'''
-accn_number =        "ACCCOUNT NUMBER"
-product_code =       "PRODUCT CODE"
-tariff_code =        "TARIFF CODE"
-intelligent_go_url = f"https://api.octopus.energy/v1/products/{product_code}/"
-move_in_date =       "2023-10-20T00:00:00" #or use the date you would like to start getting data.
-base_url =           "https://api.octopus.energy/v1" #take care with trailing slash (don't use it).
-api_key =            "YOUR API KEY"
-today =              datetime.now()
-today_date_string =  today.strftime("%Y-%m-%dT00:00:00")
-#electricity config
-mpan =              "ELEC METER MPAN NUMBER"
-serialno =          "ELEC SERIAL NUMBER"
-#gas config
-mprn =              "GAS MPRN NUMBER"
-gas_serial_number = "GAS SERIAL NUMBER"
-start_cheap = timedelta(hours=23, minutes=30) #change these values if the cheap rate starts at a different time
-end_cheap = timedelta(hours=5, minutes=30) #change these values if the cheap rate starts at a different time
+today =                  datetime.now()
+today_date_string =      today.strftime("%Y-%m-%dT00:00:00")
 
-'''
-######################################################
-END OF MAIN CONFIG
-You still need to configure your SQL Server details on line 72
-######################################################
-'''
+start_cheap = timedelta(hours=int(_config.cheap_rate_start_time[0:2]), minutes=int(_config.cheap_rate_start_time[3:5])) #change these values if the cheap rate starts at a different time
+end_cheap = timedelta(hours=int(_config.cheap_rate_stop_time[0:2]), minutes=int(_config.cheap_rate_stop_time[3:5])) #change these values if the cheap rate starts at a different time
 
 #urls
-elec_consumption_url =    f"{base_url}/electricity-meter-points/{mpan}/meters/{serialno}/consumption/?page_size=25000&period_from={move_in_date}Z&period_to={today_date_string}Z&order_by=period"
-account_url =             f"{base_url}/accounts/{accn_number}/"
-elec_rates_url =          f"{base_url}/products/{product_code}/electricity-tariffs/{tariff_code}/standard-unit-rates/"
-elec_standingcharge_url = f"{base_url}/products/{product_code}/electricity-tariffs/{tariff_code}/standing-charges/"
-elec_day_unit_url =       f"{base_url}/products/{product_code}/electricity-tariffs/{tariff_code}/day-unit-rates/"
-elec_night_unit_url =     f"{base_url}/products/{product_code}/electricity-tariffs/{tariff_code}/night-unit-rates/"
-gas_unit_url =            f"{base_url}/v1/products/{product_code}/gas-tariffs/{tariff_code}/standing-charges/"
-gas_consumption_url =     f"{base_url}/gas-meter-points/{mprn}/meters/{gas_serial_number}/consumption/"
+intelligent_go_url =       f"{_config.base_url}/products/{_config.product_code}/"
+elec_consumption_url =     f"{_config.base_url}/electricity-meter-points/{_config.mpan}/meters/{_config.serialno}/consumption/?page_size=25000&period_from={_config.move_in_date}Z&period_to={today_date_string}Z&order_by=period"
+account_url =              f"{_config.base_url}/accounts/{_config.accn_number}/"
+elec_rates_url =           f"{_config.base_url}/products/{_config.product_code}/electricity-tariffs/{_config.tariff_code}/standard-unit-rates/"
+elec_standingcharge_url =  f"{_config.base_url}/products/{_config.product_code}/electricity-tariffs/{_config.tariff_code}/standing-charges/"
+elec_day_unit_url =        f"{_config.base_url}/products/{_config.product_code}/electricity-tariffs/{_config.tariff_code}/day-unit-rates/"
+elec_night_unit_url =      f"{_config.base_url}/products/{_config.product_code}/electricity-tariffs/{_config.tariff_code}/night-unit-rates/"
+gas_unit_url =             f"{_config.base_url}/v1/products/{_config.product_code}/gas-tariffs/{_config.tariff_code}/standing-charges/"
+gas_consumption_url =      f"{_config.base_url}/gas-meter-points/{_config.mprn}/meters/{_config.gas_serial_number}/consumption/"
 
 #retrieve accountinfo
-account_details =          requests.get(account_url, auth=(api_key,''))
-elec_consumption_details = requests.get(elec_consumption_url, auth=(api_key,''))
-elec_unit_rates =          requests.get(elec_rates_url, auth=(api_key,''))
-elec_standingcharges =     requests.get(elec_standingcharge_url, auth=(api_key,''))
-elec_day_unit_rates =      requests.get(elec_day_unit_url, auth=(api_key,''))
-elec_day_unit_rates =      requests.get(elec_night_unit_url, auth=(api_key,''))
-gas_unit_rates =           requests.get(gas_unit_url, auth=(api_key,''))
-gasconsumption =           requests.get(gas_consumption_url, auth=(api_key,''))
+account_details =          requests.get(account_url, auth=(_config.api_key,''))
+elec_consumption_details = requests.get(elec_consumption_url, auth=(_config.api_key,''))
+elec_unit_rates =          requests.get(elec_rates_url, auth=(_config.api_key,''))
+elec_standingcharges =     requests.get(elec_standingcharge_url, auth=(_config.api_key,''))
+elec_day_unit_rates =      requests.get(elec_day_unit_url, auth=(_config.api_key,''))
+elec_day_unit_rates =      requests.get(elec_night_unit_url, auth=(_config.api_key,''))
+gas_unit_rates =           requests.get(gas_unit_url, auth=(_config.api_key,''))
+gasconsumption =           requests.get(gas_consumption_url, auth=(_config.api_key,''))
 
 
 def GetElectric():
     today =              datetime.now()
     today_date_string =  today.strftime("%Y-%m-%d %H:%M:%S")
     print("Getting the data from Octopus Energy API...")
-    print(f"The time is: {today_date_string}")
 
     #connect to sql database
-    '''
-    ######################################################
-    Add SQL Server details below
-    ######################################################
-    '''
+
     db = sql.connect(
-    host="HOST ADDRESS",
-    user="SQL USER",
-    passwd="SQL PASSWORD",
-    database="SQL DATABASE NAME")
+    host =      _config.sql_host_address,
+    user =      _config.sql_user,
+    passwd =    _config.sql_password,
+    database =  _config.sql_database)
     addtodata = db.cursor()
-    '''
-    ######################################################
-    END SQL config
-    ######################################################
-    '''
 
     #get values
     standing_charges = elec_standingcharges.json()
-    standing_charge =  standing_charges['results'][0]['value_inc_vat']/100
     prices =           elec_unit_rates.json()
     highrate =         prices['results'][1]['value_inc_vat']/100
     lowrate =          prices['results'][0]['value_inc_vat']/100
@@ -92,38 +58,42 @@ def GetElectric():
     
     addtodata.execute("TRUNCATE TABLE electricity") #clear out the sql table ready for new values, 25000 results from octopus give a year of 30mins results
     for values in elec_rows['results']:
-        for value in values:
-            kwh =            values['consumption']
-            jsontime =       values['interval_start']
-            startdate =      values['interval_start'][0:10]
-            starttime =      values['interval_start'][11:19]
-            starttimestamp = startdate +" "+ starttime
-            enddate =        values['interval_end'][0:10]
-            endtime =        values['interval_end'][11:19]
-            endtimestamp =   enddate +" "+ endtime
-            #have to format as time becuase python hated me for comparing time strings
-            formattedstarttime = timedelta(hours=(int(starttime[0:2])),minutes=int(starttime[3:5]))
-            formattedendtime = timedelta(hours=(int(endtime[0:2])),minutes=int(endtime[3:5]))
-            #is it BST?
-            if len(jsontime) > 20:
-                summer = 1
+        car_charge_cost = 0
+        standing_charge = 0
+        jsontime =           values['interval_start']
+        startdate =          values['interval_start'][0:10]
+        starttime =          values['interval_start'][11:19]
+        starttimestamp =     startdate +" "+ starttime
+        enddate =            values['interval_end'][0:10]
+        endtime =            values['interval_end'][11:19]
+        endtimestamp =       enddate +" "+ endtime
+        # had to format as time becuase python hated me for comparing time strings
+        formattedstarttime = timedelta(hours=(int(starttime[0:2])),minutes=int(starttime[3:5]))
+        formattedendtime =   timedelta(hours=(int(endtime[0:2])),minutes=int(endtime[3:5]))
+        kwh =                values['consumption']
+        #is it BST?
+        if len(jsontime) > 20:
+            summer = 1
+        else:
+            summer = 0
+        #which rate to use?
+        if formattedstarttime <= start_cheap and formattedendtime > end_cheap:
+            if starttime == "12:00:00":
+                standing_charge = standing_charges['results'][0]['value_inc_vat']/100
+                cost = kwh * highrate + standing_charge
+                rateused = "On High Rate, Standing Charge Added"
             else:
-                summer = 0
-            #which rate to use?
-            if formattedstarttime <= start_cheap and formattedendtime > end_cheap:
-                if starttime == "12:00:00":
-                    cost = kwh * highrate + standing_charge
-                    rateused = "On High Rate, Standing Charge Added"
-                    car_charge_cost = 0
-                else:
-                     cost = kwh * highrate
-                     rateused = "On High Rate"
-                     car_charge_cost = 0
-            else:
-                    cost = kwh * lowrate
-                    rateused = "On Low Rate"
-                    #this car charging is not accurate, its totalling the whole use cost on low rate, but the car may not charge for all 6 hours.
-                    car_charge_cost = kwh * lowrate
+                    cost = kwh * highrate
+                    rateused = "On High Rate"
+                    standing_charge = 0
+        else:
+                cost = kwh * lowrate
+                rateused = "On Low Rate"
+                standing_charge = 0
+                if kwh >= 2.2: #a 7kw charger draws 3.5kw per 30 mins, i found in testing that nothing draws more than 2.2kw at
+                               #night apart from the car, so this for me is a reliable metric for the car being on charge. YMMV
+                    car_charge_cost = kwh * lowrate  
+                    
 
         #send the data collected to the sql table
         addtodata.execute("INSERT INTO electricity (kwh, high_price, low_price,\
@@ -132,8 +102,10 @@ def GetElectric():
     db.commit()
     print("Electrickery has been gotten")
     print(f"Last retrieved at {today_date_string}")
+    print(f"I will get fresh data at {_config.schedule_time} tomorrow.")
+    print("waiting...")
 
-schedule.every().day.at("07:30").do(GetElectric)
+schedule.every().day.at(_config.schedule_time).do(GetElectric)
 
 #run once on manual startup
 do_once = True
